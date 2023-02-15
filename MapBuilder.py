6bb8306cb0d137c96fcab21b85dfccae86e13497
2ashishmohan <52281857+2ashishmohan@users.noreply.github.com>
2023-02-15 01:51:23 +0000

    Add files via upload

--- MapBuilder.py
+++ MapBuilder.py
@@ -3,12 +3,14 @@ import random
 import numpy as np
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, OccupancyGrid
 from geometry_msgs.msg import PoseStamped
 import psutil
 import rosnode
 import tf
 import math
+import os
+
 
 class MOveBaseClient:
     def __init__(self):
@@ -20,21 +22,31 @@ class MOveBaseClient:
             rospy.logerr("Action server not available!")
         rospy.loginfo("Connected to move base server")
         self.move_goal = MoveBaseGoal()
-        self.rate = rospy.Rate(2)
+        self.rate = rospy.Rate(1)
         self.pose = PoseStamped()
+        self.map_percentage = 0.0
+        rospy.Subscriber('/summit_xl/map', OccupancyGrid, self.map_callback)
 
     def pose_callback(self, msg):
             self.pose = msg
 
+    def map_callback(self, msg):
+        num_occupied = np.count_nonzero(np.array(msg.data) > 0)
+        num_free = np.count_nonzero(np.array(msg.data) == 0)
+        map_percentage = float(num_occupied) / float(num_occupied + num_free)
+        self.map_percentage = map_percentage
+        rospy.loginfo(" Map Percentage: %f", map_percentage)
+
     def run(self):
-        while not rospy.is_shutdown():
+        while not rospy.is_shutdown() and self.map_percentage < 0.95:
             #goal = self.generate_random_goal()
             
             angle = random.uniform(0, 2*np.pi)
-            distance = random.uniform(10, 20)
-            #distance = 20
-            x_offset = distance*np.cos(angle)
-            y_offset = distance*np.sin(angle)
+            #distance = random.uniform(10, 20)
+            #x_offset = distance*np.cos(angle)
+            #y_offset = distance*np.sin(angle)
+            x_offset = random.uniform(-10,10)
+            y_offset = random.uniform(-10, 10)
             self.move_goal.target_pose.header.frame_id = "summit_xl_map"
             self.move_goal.target_pose.header.stamp = rospy.Time.now()
             self.move_goal.target_pose.pose.position.x = self.pose.pose.position.x + x_offset
@@ -51,20 +63,16 @@ class MOveBaseClient:
             self.client.send_goal(self.move_goal)
             self.client.wait_for_result()
             self.rate.sleep()
-    '''
-    def generate_random_goal(self):
-            angle = random.uniform(0, 2*np.pi)
-            distance = random.uniform(0, 1)
-            x_offset = distance*np.cos(angle)
-            y_offset = distance*np.sin(angle)
-            goal = PoseStamped()
-            goal.header = self.pose.header
-            goal.pose.position.x = self.pose.pose.position.x + x_offset
-            goal.pose.position.y = self.pose.pose.position.y + y_offset
-            goal.pose.orientation = self.pose.pose.orientation
-            return goal   
-  
-    '''
+
+        self.stop_mapping()
+
+    def stop_mapping(self):
+        nodes = rosnode.get_node_name()
+        for node in nodes:
+            if 'gmapping' in node:
+                rospy.loginfo("Stopping %s......", node)
+                os.system('rosnode kill'  + node)
+    
 
 
 if __name__ == '__main__':
